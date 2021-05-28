7a561f03df8b487977166a011140f248fdff964f
breesky <84938884+breesky@users.noreply.github.com>
2021-05-27 23:17:42 -0400

    Create jokebot.js

---
+++ jokebot.js
@@ -0,0 +1,17 @@
+function startJoke(){
+  if(
+confirm("Wanna hear a joke?")
+  ){
+  response= prompt("whats a squirrels favorite ballet?");
+if(answer.toLowerCase() === response.toLowerCase()) {
+alert("You got it") 
+} else{
+ alert("The nut-cracker!");
+}
+
+  
+}
+}
+
+var response;
+var answer = "The nut- cracker!";
